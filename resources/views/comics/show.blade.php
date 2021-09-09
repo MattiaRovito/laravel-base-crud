74d2192c35ed4a26320012748e7bc9de0527525e
MattiaR <rovitomattia91@gmail.com>
2021-09-09 15:50:13 +0200

    fine index, inizio show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,7 +4,24 @@
 
 @section('content')
 
+<div class="container">
+    <h1></h1>
 
+    <div class="row">
+        <div class="col"></div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4"></div>
+            {{-- Inserisco l'immagine --}}
+            <img src="" alt="">
+        <div class="col-md-8">
+            {{-- Inserisco la descrizione --}}
+            <p></p>
+        </div>
+    </div>
+
+</div>
 
 
 
